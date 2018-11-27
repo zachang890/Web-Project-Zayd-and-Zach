bd01564e0d907de3817f1dd702ffd664a3b52d76
zaydabudayyeh <44505848+zaydabudayyeh@users.noreply.github.com>
2018-11-27 09:33:32 -0800

    Update Website Database.sql

--- Website Database.sql
+++ Website Database.sql
@@ -5,4 +5,12 @@ create table test(
 id int auto_increment primary key,
 fName varchar(40),
 lName varchar(40),
+gender varchar(1),
+age int,
+favColor varchar(10));
+insert into test (
+fFame, lName, gender, age, favColor)
+values
+('Tom','Riddle,'m','43,'black'),
+('Harry','Potter','m',17,'blue');
 
